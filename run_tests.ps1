f0cc3e438bc75e73da3c23018dfc098454a39fb7
Nikita Tchayka <nick@booster.cloud>
2020-02-22 16:57:41 +0000

    ci: Dont publish test results

--- run_tests.ps1
+++ run_tests.ps1
@@ -5,9 +5,3 @@ if ($allOutput -clike "*FAIL*") {
   Write-Host "Exit failure"
   exit 1
 }
-
-if ($env:APPVEYOR_JOB_ID) {
-  # upload results to AppVeyor
-  $wc = New-Object 'System.Net.WebClient'
-  $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\junit.xml))
-}
