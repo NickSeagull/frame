8e6ed7ca57e2006d7be2b852b324a8f629ed8090
Nikita Tchayka <nick@booster.cloud>
2020-02-22 13:20:09 +0000

    test: Fix test runner

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,1 +1,6 @@
-& "$env:ProgramFiles\AutoHotkey\AutoHotkey.exe" ".\TestSuite.ahk" | more
+$allOutput = & "$env:ProgramFiles\AutoHotkey\AutoHotkey.exe" ".\TestSuite.ahk" | more 2>&1
+
+if ($allOutput -clike "*FAIL*") {
+  Write-Host $allOutput
+  exit 1
+}
