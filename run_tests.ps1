b1f13a72595ecec6f63442fdb29a800c0c792019
Nikita Tchayka <nick@booster.cloud>
2020-02-23 15:14:35 +0000

    ci: Upload tests to appveyor

--- run_tests.ps1
+++ run_tests.ps1
@@ -6,3 +6,9 @@ if ($allOutput -clike "*FAIL*") {
 }
 
 Write-Host "ALL TESTS PASSING"
+
+if ($env:APPVEYOR_JOB_ID) {
+  # upload results to AppVeyor
+  $wc = New-Object 'System.Net.WebClient'
+  $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\junit.xml))
+}
